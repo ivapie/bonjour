b89df6b4a567246db676487043729aa1b0022e1d
Ivan Piedra <ivapie@gmail.com>
2014-09-05 12:19:59 -0600

    Initial Node.js

---
+++ api/models/User.js
@@ -0,0 +1,21 @@
+/**
+* User.js
+*
+* @description :: TODO: You might write a short summary of how this model works and what it represents here.
+* @docs        :: http://sailsjs.org/#!documentation/models
+*/
+
+module.exports = {
+
+  attributes: {
+
+    name : { type: 'string' },
+
+    age : { type: 'integer' },
+
+    email : { type: 'email' },
+
+    email2 : { type: 'email' }
+  }
+};
+
